7791d919777f953ec602f2a3d91c69b65b529a78
Karolayne Mendes <karolaynesmendes.07@gmail.com>
2023-06-26 16:52:19 -0300

    melhoria response login

--- teachfy/src/main/java/udesc/teachfy/resource/LoginResource.java
+++ teachfy/src/main/java/udesc/teachfy/resource/LoginResource.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import udesc.teachfy.dto.Login;
 import udesc.teachfy.dto.Response;
+import udesc.teachfy.dto.Storage;
 import udesc.teachfy.model.User;
 import udesc.teachfy.service.TokenService;
 
@@ -25,13 +26,13 @@ public class LoginResource {
 	private TokenService tokenService;
 	
 	@PostMapping(path="/login")
-	public ResponseEntity<Response<String>> login(@RequestBody Login login){
+	public ResponseEntity<Response<Storage>> login(@RequestBody Login login){
 		try {
 			UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(login.email(), login.password());
 			Authentication auth = this.authenticationManager.authenticate(authToken);
 			User user = (User) auth.getPrincipal();
 			String token = tokenService.generateToken(user);
-			return new ResponseEntity(new Response("Login efetuado com sucesso", token), HttpStatus.OK);			
+			return new ResponseEntity(new Response("Login efetuado com sucesso", new Storage(user.getId(), user.getName(), token)), HttpStatus.OK);			
 		} catch (Exception except) {
 			return new ResponseEntity(new Response(except.getMessage()), HttpStatus.BAD_REQUEST);
 		}
